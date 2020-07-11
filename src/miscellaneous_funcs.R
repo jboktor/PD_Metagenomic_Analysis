caad89b66f87b396159aa72ee6c4ae83805971d2
jboktor <jboktor@caltech.edu>
2020-07-10 21:38:27 -0700

    Create a distribution sanity check function that returns a histogram and ECDF cowplot

--- src/miscellaneous_funcs.R
+++ src/miscellaneous_funcs.R
@@ -1,6 +1,6 @@
 ### Miscellaneous Functions
 
-
+############################################################################################################
 ######## p-value significance (integer to symbol function)
 
 sig_mapper <- function(pval, shh = F, porq = "p") {
@@ -19,7 +19,7 @@ sig_mapper <- function(pval, shh = F, porq = "p") {
   }
   return(sigvalue)
 }
-
+############################################################################################################
 ######## p-value significance (integer to symbol function)
 
 sig_mapper2 <- function(pval, shh = F) {
@@ -39,6 +39,7 @@ sig_mapper2 <- function(pval, shh = F) {
   return(sigvalue)
 }
 
+############################################################################################################
 sig.symbol.generator <- function(Column){
   sig.symbol <- c()
   for (i in Column){
@@ -47,18 +48,41 @@ sig.symbol.generator <- function(Column){
   return(sig.symbol)
 }
 
-
+############################################################################################################
 stop_quietly <- function() {
   opt <- options(show.error.messages = FALSE)
   on.exit(options(opt))
   stop()
 }
 
-distribution <- function(obj) {
-  microbiome::abundances(obj)
-  microbiome::meta(obj)
+############################################################################################################
+
+distribution_sanity <- function(df) {
   
+  ###' input an abundance table and displays
+  ###' a histogram and ECDF distribution of data
+  ###' - can specify if both or only one plot is desired
+  
+  abund.melt <- melt(df)
+  abund.melt <- 
+    mutate(abund.melt, group = if_else(grepl("HC", variable), "HC",
+                                if_else(grepl("PC", variable), "PC","PD")))
+  
+  histo_plot <- ggplot(abund.melt, aes(x=value, fill = group), alpha = 0.4) + 
+    theme_minimal() +
+    geom_histogram(color = "black", position="dodge") +
+    theme(axis.title.x = element_blank(),
+          legend.position = c(0.9, 0.5))
+  
+  ecdf_plot <- ggplot(abund.melt, aes(x=value, colour = group)) + stat_ecdf(geom = "step", pad = FALSE) +
+    theme_minimal() +
+    labs(y = "ECDF") +
+    theme(legend.position = c(0.9, 0.5))
+  
+  plot_grid(histo_plot,ecdf_plot, ncol = 1)
 }
+
+
 
 
 
