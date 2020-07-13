54b37f221d29134341f1117f34d69a9d08c0937a
jboktor <jboktor@caltech.edu>
2020-07-12 21:40:01 -0700

    latest updates to FW analysis

--- correlation_analysis.jl
+++ correlation_analysis.jl
@@ -25,18 +25,18 @@ meta_data_path = "files/FlashWeave_data/metadata_flashweave_friendly.csv"
 # save_network("data/Co_Abundance_Analysis/phylum_network_output.edgelist", phylum_results)
 
 
-# Load PATHWAY data files
-pathways_data_path = "files/FlashWeave_data/flashweave_friendly_pathways_relab.csv"
-####### PATHWAY Model #######
-pathways_results = learn_network(pathways_data_path, meta_data_path, sensitive=true, heterogeneous=false, verbose=true, feed_forward= true, alpha=0.05, FDR=true)
-save_network("data/Co_Abundance_Analysis/pathways_network_output.edgelist", pathways_results)
+# # Load PATHWAY data files
+# pathways_data_path = "files/FlashWeave_data/flashweave_friendly_pathways_relab.csv"
+# ####### PATHWAY Model #######
+# pathways_results = learn_network(pathways_data_path, meta_data_path, sensitive=true, heterogeneous=false, verbose=true, feed_forward= true, alpha=0.05, FDR=true)
+# save_network("data/Co_Abundance_Analysis/pathways_network_output.edgelist", pathways_results)
 
 
-# # Load ENZYME data files
-# enzymes_data_path = "files/FlashWeave_data/flashweave_friendly_enzymes_relab.csv"
-# ####### ENZYME Model #######
-# enzymes_results = learn_network(enzymes_data_path, meta_data_path, sensitive=true, heterogeneous=true, verbose=true, feed_forward= true, alpha=0.05, FDR=true)
-# save_network("data/Co_Abundance_Analysis/enzymes_network_output.edgelist", enzymes_results)
+# Load ENZYME data files
+enzymes_data_path = "files/FlashWeave_data/flashweave_friendly_enzymes_relab.csv"
+####### ENZYME Model #######
+enzymes_results = learn_network(enzymes_data_path, meta_data_path, sensitive=true, heterogeneous=true, verbose=true, feed_forward= true, alpha=0.05, FDR=true)
+save_network("data/Co_Abundance_Analysis/enzymes_network_output.edgelist", enzymes_results)
 
 
 # # Load Kegg Ortholog data files
