0aee5a967d77ed090c00a0274b0f5083db3583df
jboktor <jboktor@caltech.edu>
2020-07-17 09:12:12 -0700

    remove package loading from config_env

--- src/configure_environment.R
+++ src/configure_environment.R
@@ -13,14 +13,5 @@ install.packages(
     "ggbeeswarm", "FSA", "ggpubr", "ggsci", "ggridges", "future", "svglite", "cowplot", "coin", "EnvStats", 
     "sjlabelled", "sjmisc", "sjPlot", "nlme", "eulerr", "ggthemes", "ggforce", "huge", "Matrix"))
 
-## Load Packages into enviornment
-library(ggplot2); library(tidyverse); library(readxl);library(dplyr); library(ggrepel);library(grid);
-library(gridExtra);library(reshape2);library(plyr);library(grid);library(devtools);library(RColorBrewer);
-library(ggfortify);library(vegan);library(MASS);library(compositions);library(zCompositions);library(phyloseq);
-library(Biobase);library(viridis);library("foreach");library("doParallel");library(ggbeeswarm);
-library(FSA);library(ggpubr);library(ggsci);library(microbiome);library(ggridges);library(future);library(cowplot);
-library(EnvStats);library(sjlabelled);library(sjmisc);library(sjPlot);library(nlme);library(eulerr);library(ggthemes);
-library(ggforce);library(huge);library(Matrix)
-
 
 
