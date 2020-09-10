fa0b1b9a426725347cb07b009190ec60fe073833
jboktor <jboktor@caltech.edu>
2020-09-10 10:26:20 -0700

    Shiny App - Exploratory Analysis of Dataset Features

--- EDA_App/app.R
+++ EDA_App/app.R
@@ -2,25 +2,35 @@
 # This is a Shiny web application. You can run the application by clicking
 # the 'Run App' button above.
 #
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
+library(rsconnect)
 library(DT)
+library(BiocManager)
+options(repos = BiocManager::repositories())
 
-rootdir = substr(getwd(), 1, nchar(getwd()) -7)
+source("miscellaneous_funcs_EDA.R")
+source("load_packages_EDA.R")
 
-source(paste0(rootdir, "src/miscellaneous_funcs.R"))
-source(paste0(rootdir, "src/load_packages.R"))
+load("PhyloseqObj.RData")
+Phylo_Objects
+
+# rootdir = substr(getwd(), 1, nchar(getwd()) -7)
+# source(paste0(rootdir, "src/miscellaneous_funcs.R"))
+# source(paste0(rootdir, "src/load_packages.R"))
 # source(paste0(rootdir, "src/load_phyloseq_obj.R"))
 
 
 
 ui <- fluidPage(
     titlePanel("Explore Dataset"),
-    h4("Instructions: .."),
+    h4("Instructions: "),
+    h6("1) Select a dataset of interest from drop-down menu below.", 
+       br(), br(),
+       "2) When table loads, you may search for particular features and copy and paste them into the textbox below. ", 
+       br(), br(),
+       "Suggested Normalization: TSS ", br(),
+       "Suggested Transformation: ArcsinSqrt"),
     sidebarLayout(
         sidebarPanel(
             selectInput("phyloseqInput", "Dataset",
@@ -35,16 +45,24 @@ ui <- fluidPage(
             radioButtons("transformationInput", "Transformation",
                          choices = c("ArcSinSqrt", "None"),
                          selected = "None"),
-            textInput("featureInput","Input feature to plot:")
+            textInput("featureInput","Manually Input feature to plot:")
         ),
-        mainPanel(h4("Data Distribution"),
-                  plotOutput("distribution_plot"),
-                  br(), br(),
-                  DT::dataTableOutput("table_results"),
-                  br(),
-                  h4("Feature of interest"),
-                  plotOutput("boxplot"),
-                  br(), br(), br(), br(), br(), br(), br(), br() )
+        mainPanel(
+            tabsetPanel(
+                tabPanel("Data Distribution", plotOutput("distribution_plot")), 
+                tabPanel("Table Results",  DT::dataTableOutput("table_results")), 
+                tabPanel("Feature of Interest", plotOutput("boxplot"))
+            )
+            
+    #         h4("Data Distribution"),
+    #               plotOutput("distribution_plot"),
+    #               br(), br(),
+    #               DT::dataTableOutput("table_results"),
+    #               hr(),
+    #               h4("Feature of interest"),
+    #               plotOutput("boxplot"),
+    #               br(), br(), br(), br(), br(), br(), br(), br()
+        )
     )
 ) 
 
@@ -75,19 +93,81 @@ server <- function(input, output) {
 
 
     })
-    output$boxplot <- renderPlot({
+    
+    output$table_results <- 
+        DT::renderDataTable(server = FALSE, selection = 'single', {
+    
         
-        # generate bins based on input$bins from ui.R
-        datObj <- Phylo_Objects[[input$phyloseqInput]]
+        if (input$normalizationInput == "TSS") {
+            Phylo_Objects[[input$phyloseqInput]] %>% 
+                microbiome::transform("compositional") %>% 
+                microbiome::abundances()
+            
+        } else if (input$normalizationInput == "CLR") {
+            Phylo_Objects[[input$phyloseqInput]] %>% 
+                microbiome::transform("clr") %>% 
+                microbiome::abundances()
+        } else {
+            Phylo_Objects[[input$phyloseqInput]] %>% 
+                microbiome::abundances()
+        }
         
-        # This method doesn't include transfm. yet
-        plot_feature(datObj, input$featureInput)
     })
     
-    output$table_results <- DT::renderDataTable({
-        Phylo_Objects[[input$phyloseqInput]] %>% 
-            microbiome::abundances()
+    output$boxplot <- renderPlot({
+        
+        s = input$table_results_rows_selected
+        
+        if (length(s)) {
+            
+            
+            
+            # datObj <- Phylo_Objects[[input$phyloseqInput]]
+            
+            
+            # plot_feature(datObj, input$featureInput)
+            
+            if (input$normalizationInput == "TSS") {
+                abund <- Phylo_Objects[[input$phyloseqInput]] %>% 
+                    microbiome::transform("compositional") %>% 
+                    microbiome::abundances() %>% 
+                    as.data.frame() %>% 
+                    rownames_to_column() 
+                
+            } else if (input$normalizationInput == "CLR") {
+                abund <- Phylo_Objects[[input$phyloseqInput]] %>% 
+                    microbiome::transform("clr") %>% 
+                    microbiome::abundances() %>% 
+                    as.data.frame() %>% 
+                    rownames_to_column() 
+                
+            } else {
+                abund <- Phylo_Objects[[input$phyloseqInput]] %>% 
+                    microbiome::abundances() %>% 
+                    as.data.frame() %>% 
+                    rownames_to_column() 
+            }
+            
+            
+            # Select Feature from abundance df
+            feature <- abund$rowname[s]
+            d <- abund %>% 
+                filter(rowname == feature) %>% 
+                melt()
+            
+            dm <-  group_col_from_ids(d, id=d$variable)
+            dm$value <- asin(sqrt(dm$value))
+            dm$group <- factor(dm$group, levels = c("PC", "PD", "HC"))
+            boxplot_all(dm, x=dm$group, y=dm$value, 
+                        cols=c("PC"= "#bfbfbf", 
+                               "PD" = "#ed7d31", 
+                               "HC" = "#5b9bd5"), 
+                        title=" ", 
+                        ylabel= paste(unique(dm$rowname), "Abundance"))
+        }
+        
     })
+    
     
 } 
 
---
+++ EDA_App/load_packages_EDA.R
@@ -0,0 +1,13 @@
+# Load Packages
+
+## Load Packages into enviornment
+library(ggplot2); library(tidyverse);library(dplyr); library(ggrepel);library(grid);
+library(gridExtra);library(reshape2);library(plyr);library(grid);library(devtools);library(RColorBrewer);
+library(ggfortify);library(vegan);library(MASS);
+library(phyloseq);
+library(Biobase);
+library(ggbeeswarm);
+library(FSA);library(ggpubr);library(ggsci);
+library(microbiome);library(cowplot)
+
+
