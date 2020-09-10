83c92a186677a535c2ff441eb080fd10617993c6
jboktor <jboktor@caltech.edu>
2020-09-10 10:21:14 -0700

    adds packages

--- src/configure_environment.R
+++ src/configure_environment.R
@@ -2,19 +2,76 @@
 
 #### Install Bioconductor Packages
 install.packages("BiocManager")
-BiocManager::install(
-  c("phyloseq", "microbiome", "Biobase", "Maaslin2", "DirichletMultinomial"))
+BiocManager::install(c(
+  "phyloseq",
+  "microbiome",
+  "Biobase",
+  "Maaslin2",
+  "DirichletMultinomial"
+))
 
 
 #### Install CRAN Packages
 install.packages(
-  c("ggplot2", "tidyverse", "readxl", "dplyr", "ggrepel", "gridExtra", "reshape2", "plyr", 
-    "devtools", "RColorBrewer", "ggfortify", "vegan", "MASS", "compositions", "zCompositions",
-    "gplots", "viridis", "lme4", "phangorn", "plotly", "VennDiagram", "viridis","foreach", "doParallel" , 
-    "ggbeeswarm", "FSA", "ggpubr", "ggsci", "ggridges", "future", "svglite", "cowplot", "coin", "EnvStats", 
-    "sjlabelled", "sjmisc", "sjPlot", "nlme", "eulerr", "ggthemes", "ggforce", "huge", "Matrix", "magrittr",
-    "randomForest", "pROC", "SGL", "rstatix"))
+  c(
+    "ggplot2",
+    "tidyverse",
+    "readxl",
+    "dplyr",
+    "ggrepel",
+    "gridExtra",
+    "reshape2",
+    "plyr",
+    "devtools",
+    "RColorBrewer",
+    "ggfortify",
+    "vegan",
+    "MASS",
+    "compositions",
+    "zCompositions",
+    "gplots",
+    "viridis",
+    "lme4",
+    "phangorn",
+    "plotly",
+    "VennDiagram",
+    "viridis",
+    "foreach",
+    "doParallel" ,
+    "parallel",
+    "ggbeeswarm",
+    "FSA",
+    "ggpubr",
+    "ggsci",
+    "ggridges",
+    "future",
+    "svglite",
+    "cowplot",
+    "coin",
+    "EnvStats",
+    "sjlabelled",
+    "sjmisc",
+    "sjPlot",
+    "nlme",
+    "eulerr",
+    "ggthemes",
+    "ggforce",
+    "huge",
+    "Matrix",
+    "magrittr",
+    "randomForest",
+    "pROC",
+    "plotROC",
+    "SGL",
+    "rstatix",
+    "mlbench",
+    "caret",
+    "MLeval"
+  )
+)
+
 
 #### Install Other Github available Packages
 install.packages("remotes")
 remotes::install_github("gmteunisse/Fantaxtic")
+remotes::install_github("schuyler-smith/phyloschuyler")
--- src/load_packages.R
+++ src/load_packages.R
@@ -1,14 +1,70 @@
 # Load Packages
 
 ## Load Packages into enviornment
-library(ggplot2); library(tidyverse); library(readxl);library(dplyr); library(ggrepel);library(grid);
-library(gridExtra);library(reshape2);library(plyr);library(grid);library(devtools);library(RColorBrewer);
-library(ggfortify);library(vegan);library(MASS);library(compositions);library(zCompositions);library(phyloseq);
-library(Biobase);library(viridis);library("foreach");library("doParallel");library(ggbeeswarm);
-library(FSA);library(ggpubr);library(ggsci);library(microbiome);library(ggridges);library(future);library(cowplot);
-library(EnvStats);library(sjlabelled);library(sjmisc);library(sjPlot);library(nlme);library(eulerr);library(ggthemes);
-library(ggforce);library(huge);library(Matrix);library(Maaslin2);library(fantaxtic);library(igraph);library(slam);
-library(scales);library(DirichletMultinomial);library(magrittr);library(randomForest);library(pROC);library(SGL);
+library(ggplot2)
+library(tidyverse)
+library(readxl)
+library(dplyr)
+library(ggrepel)
+library(grid)
+
+library(gridExtra)
+library(reshape2)
+library(plyr)
+library(grid)
+library(devtools)
+library(RColorBrewer)
+
+library(ggfortify)
+library(vegan)
+library(MASS)
+library(compositions)
+library(zCompositions)
+library(phyloseq)
+
+library(Biobase)
+library(viridis)
+library(ggbeeswarm)
+
+library(FSA)
+library(ggpubr)
+library(ggsci)
+library(microbiome)
+library(ggridges)
+library(future)
+library(cowplot)
+
+library(EnvStats)
+library(sjlabelled)
+library(sjmisc)
+library(sjPlot)
+library(nlme)
+library(eulerr)
+library(ggthemes)
+
+library(ggforce)
+library(huge)
+library(Matrix)
+library(Maaslin2)
+library(fantaxtic)
+library(igraph)
+library(slam)
+
+library(scales)
+library(DirichletMultinomial)
+library(magrittr)
+library(randomForest)
+library(pROC)
+library(plotROC)
+library(SGL)
+
 library(rstatix)
+library(parallel)
+library(doParallel)
+library(foreach)
+library(mlbench)
 
+library(caret)
+library(MLeval)
 
+# library(phylosmith)
