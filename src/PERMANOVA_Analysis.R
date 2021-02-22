4a810a9734e3dea200ff2e6102ebd0ce0d65d25a
Joseph Boktor <josephboktor@Josephs-MacBook-Pro.local>
2021-02-22 11:23:30 -0800

    PERMANOVA analysis

--- src/PERMANOVA_Analysis.R
+++ src/PERMANOVA_Analysis.R
@@ -1,32 +1,29 @@
 ## PERMANOVA script
 
-library(ggplot2); library(tidyverse); library(readxl);library(dplyr); library(ggrepel);library(grid);
-library(gridExtra);library(reshape2);library(plyr);library(grid);library(devtools);library(RColorBrewer);
-library(ggfortify);library(vegan);library(MASS);library(compositions);library(zCompositions);library(phyloseq);
-library(Biobase);library(viridis);library("foreach");library("doParallel");library(ggbeeswarm);
-library(FSA);library(ggpubr);library(ggsci);library(microbiome);library(ggridges);library(future);library(cowplot)
+rm(list = ls())
+source("src/load_packages.R")
+source("src/miscellaneous_funcs.R")
+source("src/load_phyloseq_obj.R")
+source("src/metadata_prep_funcs.R")
+load("files/low_quality_samples.RData")
+load_all_cohorts()
 
-
-load("files/Species_PhyloseqObj.RData")
-load("files/KOs.all_PhyloseqObj.RData")
-
-dat <- microbiome::transform(dat, "compositional")
-
+dat <- dat.species %>% 
+  subset_samples(donor_id %ni% low_qc[[1]]) %>% 
+  microbiome::transform("compositional")
 dat_clr <- microbiome::transform(dat, "clr")
 sp <- t(abundances(dat_clr))
 
 # Run Metadata pre-processing function
-source("src/Metadata_prep_funcs.R")
-process_meta(dat)
-trim_meta(env)
-
+env <- process_meta(dat, cohort = "Merged")
+env <- trim_meta(env, min_ratio = 2/234)
+# remove ID variable and duplicate grouping variable
 env.pa <- dplyr::select(env, -matches(other_metadata))
-
 ## Adding Alpha Diversity to PERMANOVA
 env.pa$Shannon <- alpha(abundances(dat), 'shannon')$diversity_shannon
+# env$right_hand
 
-
-####################  Prep Permanova  #################### 
+####################  Prep PERMANOVA  #################### 
 distlist <- c("euclidean")
 sigmetadata <- vector("list", length(distlist))
 names(sigmetadata) = distlist
@@ -39,10 +36,11 @@ siglst <- c()
 env.pa_perm <- tibble()
 
 #################### PERMANOVA LOOP  #################### 
-# Note with permuations = 9999, will take 5-10 mins to run
+# Note with permutations = 9999, will take 5-10 min to run
 
 # Runs through metadata columns, if NA detected, 
-# sample is omitted from metdata & same sample from abundnace table
+# sample is omitted from metadata & same sample from abundance table
+
 for (i in 1:length(env.pa)) { 
   a <- env.pa[,i]
   a.narm <- na.omit(a)
@@ -53,7 +51,7 @@ for (i in 1:length(env.pa)) {
   }
   for (j in distlist) {
     meta_ano = adonis(dist(sp.narm, method = "euclidean") ~ a.narm, permutations = 9999)
-    row2add <- cbind(colnames(env.pa[i]),meta_ano$aov.tab[1,],length(a.narm))
+    row2add <- cbind(colnames(env.pa[i]) ,meta_ano$aov.tab[1,], length(a.narm))
     env.pa_perm <- rbind(env.pa_perm, row2add)
     sig <- meta_ano$aov.tab$`Pr(>F)`[1]
     if (sig < .1 & sig > .05) {
@@ -71,68 +69,104 @@ for (i in 1:length(env.pa)) {
 
 
 ####################  VARS  - For Plotting & Confounder Analysis #################### 
-# 
 permdf <- env.pa_perm
-permdf$FDR <- p.adjust(permdf$`Pr(>F)`, method = 'BH', n = nrow(permdf)) # Multiple comparison correction using Benjamini Hochberg Method
+# Multiple comparison correction using Benjamini Hochberg Method
+permdf$FDR <- p.adjust(permdf$`Pr(>F)`, method = 'BH')
 rownames(permdf) <- NULL
 colnames(permdf)[1] <- "vars"
 colnames(permdf)[8] <- "n_meta"
 permdf$R2 <- permdf$R2*100
 
+
 ## Add Metadata catagory column
-permdf <- mutate(permdf, metacat = if_else(permdf$vars %in% grouping, "Disease-Status/Grouping",
-                                           if_else(permdf$vars %in% Anthro, "Anthropomorphic",
-                                                   if_else(permdf$vars %in% GI, "GI distress",
-                                                           if_else(permdf$vars %in% nonstarters, "Microbiota disruption",
-                                                                   if_else(permdf$vars %in% allergy, "Allergies",
-                                                                           if_else(permdf$vars %in% smoke, "Smoking stats",
-                                                                                   if_else(permdf$vars %in% general_drugs, "Other medication",
-                                                                                           if_else(permdf$vars %in% pd_drugs, "PD medication",
-                                                                                                   if_else(permdf$vars %in% supplements, "Supplements",
-                                                                                                           if_else(permdf$vars %in% diet, "Diet",
-                                                                                                                   if_else(permdf$vars == "Shannon", "Shannon Diversity", 
-                                                                                                                           "AA_notmatched"))))))))))))
-
-
+permdf <-
+  mutate(permdf,
+         metacat = if_else(
+           permdf$vars %in% grouping,
+           "Disease-Status/Grouping",
+           if_else(
+             permdf$vars %in% Anthro,
+             "Anthropomorphic",
+             if_else(
+               permdf$vars %in% GI,
+               "GI distress",
+               if_else(
+                 permdf$vars %in% nonstarters,
+                 "Microbiota disruption",
+                 if_else(
+                   permdf$vars %in% allergy,
+                   "Allergies",
+                   if_else(
+                     permdf$vars %in% smoke,
+                     "Smoking",
+                     if_else(
+                       permdf$vars %in% general_drugs,
+                       "Other medication",
+                       if_else(
+                         permdf$vars %in% pd_drugs,
+                         "PD medication",
+                         if_else(
+                           permdf$vars %in% supplements,
+                           "Supplements",
+                           if_else(
+                             permdf$vars %in% diet,
+                             "Diet",
+                             if_else(
+                               permdf$vars %in% motor_severity_scores_summary,
+                               "Motor Symptom Severity",
+                               if_else(
+                                 permdf$vars %in% clinical_variables,
+                                 "Clinical Variables",
+                                 if_else(
+                                   permdf$vars %in% environmental,
+                                   "Environment",
+                                   if_else(permdf$vars == "Shannon", "Shannon Diversity",
+                                           "AA_notmatched")
+                                 )
+                               )
+                             )
+                           )
+                         )
+                       )
+                     )
+                   )
+                 )
+               )
+             )
+           )
+         ))
 
 write.csv(permdf, file = 'files/permanova_data.csv')
 
-####################  FILTERING FOR FDR SIGNIFICANT 
-
+# FILTERING FOR FDR SIGNIFICANT
 permdfsig <- filter(permdf, FDR <= 0.05)
 
-
-#################### FILTERING FOR CORE METADATA - n > 90 (Metadata present with most samples)
-
+# FILTERING FOR CORE METADATA - n > 90 (Metadata present with most samples)
 permdfcore <- filter(permdf, n_meta >= 90)
 permdfcoresig <- filter(permdfcore, FDR <= 0.5)
 
-
-#################### FILTERING FOR Essential METADATA
-
-essential <- c("Disease-Status/Grouping", "Anthropomorphic", "GI distress", "Microbiota disruption", "Shannon Diversity")
+# FILTERING FOR Essential METADATA
+essential <-
+  c(
+    "Disease-Status/Grouping",
+    "Anthropomorphic",
+    "GI distress",
+    "Microbiota disruption",
+    "Shannon Diversity"
+  )
 permdfess <- filter(permdf, metacat %in% essential)
 
-
-#################### FILTERING FOR DIET METADATA
-
+# FILTERING FOR DIET METADATA
 permdf_diet <- filter(permdf, metacat == "Diet")
 
-
-#################### FILTERING FOR Supplement METADATA
-
+# FILTERING FOR Supplement METADATA
 permdf_supp <- filter(permdf, metacat == "Supplements")
 
-
-#################### FILTERING FOR General Drug use METADATA
-
+# FILTERING FOR General Drug use METADATA
 permdf_gendrug <- filter(permdf, metacat == "Other medication")
 
-
-#################### FILTERING FOR PD Drug use METADATA
-
+# FILTERING FOR PD Drug use METADATA
 permdf_pddrug <- filter(permdf, metacat == "PD medication")
-
 
 
 
--- src/PERMANOVA_VIz.R
+++ src/PERMANOVA_VIz.R
@@ -1,21 +1,25 @@
 # PERMANOVA PLOTS
 
-library(ggplot2); library(tidyverse); library(readxl);library(dplyr); library(ggrepel);library(grid);
-library(gridExtra);library(reshape2);library(plyr);library(grid);library(devtools);library(RColorBrewer);
-library(ggfortify);library(vegan);library(MASS);library(compositions);library(zCompositions);library(phyloseq);
-library(Biobase);library(viridis);library("foreach");library("doParallel");library(ggbeeswarm);
-library(FSA);library(ggpubr);library(ggsci);library(microbiome);library(ggridges);library(future);library(cowplot)
+# source("src/load_packages.R")
+# source("src/miscellaneous_funcs.R")
+# source("src/load_phyloseq_obj.R")
+# source("src/metadata_prep_funcs.R")
+# load("files/low_quality_samples.RData")
+# load_all_cohorts()
 
 source("src/PERMANOVA_Analysis.R")
-source("src/miscellaneous_funcs.R")
 
 
-####################   PLOTTING #################### 
+#-------------------------------------------------------------------------------
+#####                           Plotting                                 ##### 
+#-------------------------------------------------------------------------------
 
 permdf <- permdf %>% filter(metacat != "AA_notmatched")
-
-#################### All Metadata - pvalue fill
+# All Metadata - pvalue fill
 permdf$FDR.symbol <- sig.symbol.generator(permdf$FDR)
+permdf.q25 <- permdf %>% filter(FDR <= 0.25)
+permdf.q50 <- permdf %>% filter(FDR <= 0.5)
+permdf.q75 <- permdf %>% filter(FDR <= 0.75)
 
 p1 <- ggplot() +
   geom_col(data=permdf, aes(x=R2, y=reorder(vars, -FDR), fill=`FDR`)) +
@@ -29,33 +33,48 @@ p1 <- ggplot() +
   theme(axis.title.y = element_blank(),
         legend.position = c(.90, .50))
 p1
-ggsave("data/PERMANOVA_Analysis/PERMANOVA_Barplt_All_Metdata_Aitchisons.svg", height = 10, width =10)
+# ggsave("data/PERMANOVA_Analysis/PERMANOVA_Barplt_All_Metdata_Aitchisons.svg", height = 10, width =10)
 
-#################### All Metadata - METADATA CATEGORY FILL
-p2 <- ggplot() +
+
+# All Metadata - METADATA CATEGORY FILL
+p2 <- 
+  ggplot() +
   geom_col(data=permdf, aes(x=R2, y=reorder(vars, -FDR), fill=metacat)) +
   theme_bw() +
-  # labs(x="Variance Explained - Adjusted R^2 %", fill = "Adjusted\np-value") +
   labs(x=expression("Variance Explained - " ~ R^2~ "(%)"), fill = "Adjusted\np-value") +
-  # ggtitle("PERMANOVA Covariate Independence") +
   annotate("text", y = unique(permdf$vars), x = (permdf$R2+10), label = permdf$FDR.symbol, size = 2) +
   annotate("text", y = unique(permdf$vars), x = (permdf$R2+3), label = paste0("n =", permdf$n_meta), size = 2) +
-  # scale_fill_viridis(direction=-1, option="cividis")+
   scale_fill_simpsons()+
   theme(axis.title.y = element_blank(),
         panel.grid.minor  = element_blank(),
         legend.position = c(.80, .50))
 p2
-ggsave("data/PERMANOVA_Analysis/PERMANOVA_Barplt_All_Metdata_Aitchisons_metacatfill.svg", height = 10, width =10)
+# ggsave("data/PERMANOVA_Analysis/PERMANOVA_Barplt_All_Metdata_Aitchisons_metacatfill.svg", height = 10, width =10)
 
 
+# Metadata with q value above 0.25- METADATA CATEGORY FILL
 
+p2.2 <- 
+  ggplot(data = permdf.q25) +
+  geom_col(aes(x=R2, y=reorder(vars, -FDR), fill=metacat), width = 0.6) +
+  theme_bw() +
+  labs(x=expression("Variance Explained - " ~ R^2~ "(%)"), fill = "Metadata\nCategory") +
+  annotate("text", y = unique(permdf.q25$vars), x = (permdf.q25$R2+6), label = permdf.q25$FDR.symbol, size = 3) +
+  annotate("text", y = unique(permdf.q25$vars), x = (permdf.q25$R2+3), label = paste0("n =", permdf.q25$n_meta), size = 3) +
+  scale_fill_simpsons() +
+  theme(axis.title.y = element_blank(),
+        panel.grid.minor  = element_blank(),
+        panel.grid.major.y = element_blank())
+p2.2
+# ggsave(p2.2, filename = "data/PERMANOVA_Analysis/PERMANOVA_Barplt_Q0.25_Aitchisons_metacatfill.svg", height = 5, width = 13)
 
 
 ###########################################   FIGURE 1 Plots #########################################################
 
 ############# Metadata Category Summary - Fill by p-Value
-p3 <- ggplot() + geom_col(data=permdf, aes(x=R2, y=reorder(metacat, R2), fill=FDR)) +
+p3 <- 
+  ggplot(data = permdf.q50) +
+  geom_col(aes(x=R2, y=reorder(metacat, R2), fill=FDR), width = 0.6) +
   theme_bw() +
   ggtitle("Cumulative Metadata Explained Variance") +
   theme(plot.title = element_text(hjust = 0.5))+
@@ -65,7 +84,7 @@ p3 <- ggplot() + geom_col(data=permdf, aes(x=R2, y=reorder(metacat, R2), fill=FD
         panel.grid.major.y = element_blank(),
         panel.grid.minor.x = element_blank())
 p3
-ggsave("data/PERMANOVA_Analysis/PERMANOVA_Barplt_Cumulative_All_Metdata_Aitchisons.svg", height = 4, width =10)
+ggsave(p3, filename = "data/PERMANOVA_Analysis/PERMANOVA_Barplt_Cumulative_Q0.25_Aitchisons.svg", height = 3, width =5)
 
 #################### FILTERING FOR Essential METADATA
 
@@ -85,10 +104,10 @@ p6 <- ggplot() +
         panel.grid.major.y = element_blank(),
         panel.grid.minor.x = element_blank())
 p6
-ggsave("data/PERMANOVA_Analysis/PERMANOVA_Barplt_essential_Metdata_Aitchisons.svg", height = 4, width =10)
+# ggsave("data/PERMANOVA_Analysis/PERMANOVA_Barplt_essential_Metdata_Aitchisons.svg", height = 4, width =10)
 
 c1 <- cowplot::plot_grid(p3, p6, ncol=2, align = "hv")
-ggsave(c1, filename = "figures/Figure_1/PERMANOVA_Analysis.svg", height = 3.25, width =20)
+# ggsave(c1, filename = "figures/Figure_1/PERMANOVA_Analysis.svg", height = 3.25, width =20)
 
 
 ####################################################################################################
@@ -99,8 +118,7 @@ ggsave(c1, filename = "figures/Figure_1/PERMANOVA_Analysis.svg", height = 3.25, 
 
 ####################  FILTERING FOR FDR SIGNIFICANT 
 
-# permdfsig <- filter(permdf, FDR <= 0.05)
-
+permdfsig <- filter(permdf, FDR <= 0.05)
 p4 <- ggplot() +
   geom_col(data=permdfsig, aes(x=R2, y=reorder(vars, R2), fill=FDR)) +
   theme_bw() +
@@ -110,6 +128,7 @@ p4 <- ggplot() +
   annotate("text", y = unique(permdfsig$vars), x = (permdfsig$R2+7), label = paste0("n =", permdfsig$n_meta), size = 2) +
   scale_fill_viridis(direction=-1, option="cividis")+
   theme(axis.title.y = element_blank())
+p4
 # ggsave("data/PERMANOVA_Analysis/PERMANOVA_Barplt_Significant_Metdata_Aitchisons.svg", height = 4, width =10)
 
 
@@ -130,6 +149,7 @@ p5 <- ggplot() +
            x = (permdfcoresig$R2+1.5), label = paste0("n =", permdfcoresig$n_meta), size = 2) +
   scale_fill_viridis(direction=-1, option="cividis")+
   theme(axis.title.y = element_blank())
+p5
 # ggsave("data/PERMANOVA_Analysis/PERMANOVA_Barplt_CoreFeatures_Metdata_Aitchisons.svg", height = 4, width =10)
 
 
@@ -152,6 +172,7 @@ e1 <- ggplot() +
   annotate("text", y = unique(permdf_diet$vars), x = (permdf_diet$R2+1), label = paste0("n =", permdf_diet$n_meta), size = 3) +
   scale_fill_viridis(direction=-1, option="cividis")+
   theme(axis.title.y = element_blank())
+e1
 # ggsave("data/PERMANOVA_Analysis/PERMANOVA_Barplt_Diet_Metdata_Aitchisons.svg", height = 5, width =10)
 
 
@@ -169,6 +190,7 @@ e2 <- ggplot() +
   annotate("text", y = unique(permdf_supp$vars), x = (permdf_supp$R2+1), label = paste0("n =", permdf_supp$n_meta), size = 3) +
   scale_fill_viridis(direction=-1, option="cividis")+
   theme(axis.title.y = element_blank())
+e2
 # ggsave("data/PERMANOVA_Analysis/PERMANOVA_Barplt_Supplements_Metdata_Aitchisons.svg", height = 5, width =10)
 
 
